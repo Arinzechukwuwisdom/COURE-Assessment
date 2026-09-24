94c30833ca3be13aaeaec5e86a5fab96aab83d09
Arinzechukwu Ogbu <arinzechukwuwisdomogbu@gmail.com>
2026-09-24 14:11:54 +0100

    Refixed the code

--- Program.cs
+++ Program.cs
@@ -1,30 +1,40 @@
-﻿using System.Threading.Channels;
+﻿while (true)
+{
+    Console.WriteLine("Enter numbers separated by spaces (e.g., 1 2 3 4 5 or 8 2 3):");
 
-        Console.WriteLine("Enter numbers separated by spaces (e.g., 1 2 3 4 5 or 8 2 3):");
-        string input = Console.ReadLine();
+    string input = Console.ReadLine();
 
-        string[] stringArray = input.Split(' ');
-        int[] numbers = Array.ConvertAll(stringArray, Convert.ToInt32);
+    string[] stringArray = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-        int totalScore = 0;
+    int totalScore = 0;
+    bool validInput = true;
 
-        foreach (int num in numbers)
+    foreach (string item in stringArray)
+    {
+        if (!int.TryParse(item, out int num))
         {
-            if (num == 8)
-            {
-                totalScore += 5;
-            }
-            else if (num % 2 == 0)
-            {
-                totalScore += 1;
-            }
-            else
-            {
-                totalScore += 3;
-            }
+            Console.WriteLine($"'{item}' is not a valid number. Please try again.");
+            validInput = false;
+            break;
         }
+
+        if (num == 8)
+        {
+            totalScore += 5;
+        }
+        else if (num % 2 == 0)
+        {
+            totalScore += 1;
+        }
+        else
+        {
+            totalScore += 3;
+        }
+    }
+
+    if (validInput)
+    {
         Console.WriteLine($"\nTotal Score: {totalScore}");
-    
-
-
-
+        break;
+    }
+}
